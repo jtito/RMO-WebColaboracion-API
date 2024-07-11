e4a4d444aaba9c87d19c543887f24a7022aec215
Anaiz Rojas <ani.rojas.sen@gmail.com>
2024-07-11 12:18:24 -0500

    Login Validation Password and Email

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Usuario
+from django.contrib.auth.hashers import check_password
 
 # Create your views here.
 
@@ -15,10 +17,23 @@ class UsuarioView(ModelViewSet):
 
 class LoginView(APIView):
     def post(self, request, *args, **kwargs):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            email = serializer.validated_data["email"]
-            password = serializer.validated_data["password"]
+        user_request = LoginSerializer(data=request.data)
+        if not user_request.is_valid():
+            return Response({"message": user_request.errors}, status=401)
+        
+        user = Usuario.objects.get(email=user_request.data["email"])
 
-            return Response({"message": "Login exitoso"}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if not user:
+            return Response({"message": "Email y/o password incorrectos"}, status=401)
+        user_serializer = UsuarioSerializer(user).data
+        
+        if not check_password(
+            user_request.data["password"], user_serializer.get("password")
+        ):
+            return Response({"message": "Email y/o password incorrectos"}, status=401)
+
+        return Response(
+            {
+                "user": user_serializer,
+            }
+        )
