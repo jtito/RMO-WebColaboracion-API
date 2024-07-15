0d33e82a728c15ec63329132880d195bfdf9efba
Anaiz Rojas <ani.rojas.sen@gmail.com>
2024-07-15 14:43:57 -0500

    JWT para el login

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,9 +3,9 @@ from .serializers import UsuarioSerializer, LoginSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from .models import Usuario
 from django.contrib.auth.hashers import check_password
+from .utils import get_tokens_for_user
 
 # Create your views here.
 
@@ -20,20 +20,23 @@ class LoginView(APIView):
         user_request = LoginSerializer(data=request.data)
         if not user_request.is_valid():
             return Response({"message": user_request.errors}, status=401)
-        
+
         user = Usuario.objects.get(email=user_request.data["email"])
 
         if not user:
             return Response({"message": "Email y/o password incorrectos"}, status=401)
         user_serializer = UsuarioSerializer(user).data
-        
+
         if not check_password(
             user_request.data["password"], user_serializer.get("password")
         ):
             return Response({"message": "Email y/o password incorrectos"}, status=401)
+        tokens = get_tokens_for_user(user)
 
         return Response(
             {
                 "user": user_serializer,
+                "access_token": tokens["access"],
+                "refresh_token": tokens["refresh"],
             }
         )
