eaa1857b1ef501e67728465262f893a181cb4c30
fio2002 <Pi76447366@idat.edu.pe>
2024-07-15 11:02:54 -0500

    cors

--- RMO/settings.py
+++ RMO/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "drf_yasg",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = "RMO.urls"
@@ -81,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "CRIFCAN",
         "USER": "postgres",
-        "PASSWORD": "1234",
+        "PASSWORD": "122112",
         "HOST": "localhost",
         "PORT": "5432",
     }
@@ -130,3 +132,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3001",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
