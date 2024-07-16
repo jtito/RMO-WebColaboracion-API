7cb7f22b31b18939c5cdeebdd6fda6e8cb5059f2
Anaiz Rojas <ani.rojas.sen@gmail.com>
2024-07-16 16:41:52 -0500

    values & endpoints

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,14 +1,21 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserView, LoginView
+from .views import UserView, LoginView, CountryChoicesView,RoleChoicesView,TypeDocChoicesView
 from django.urls import path
 
 router = DefaultRouter()
 router.register(r"usuarios", UserView, basename="usuario")
 
 
+custom_urlpatterns = [
+    path("role/",RoleChoicesView.as_view(),name="role-choices"),
+    path("countries/",CountryChoicesView.as_view(),name="country-choices"),
+    path("typedocs/",TypeDocChoicesView.as_view(),name="typedocs-choices"),
+]
+
+
 #cuando es APIVIEW agregamos aqui
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-]
+]+ custom_urlpatterns
 
 urlpatterns += router.urls
