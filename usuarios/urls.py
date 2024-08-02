35cf9d672096c0d66d947584e7e7a299a03a704c
MichelleFMB <102604395+MichelleFMB@users.noreply.github.com>
2024-08-02 15:39:54 -0500

    fix - modification urls

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,20 +1,19 @@
 from rest_framework.routers import DefaultRouter
-from .views import UserView, LoginView, CountryChoicesView,TypeDocChoicesView
+from .views import UserView, LoginView, CountryChoicesView, TypeDocChoicesView, PasswordResetRequestView, PasswordResetView
 from django.urls import path
 
 router = DefaultRouter()
-router.register(r"usuarios", UserView, basename="usuario")
-
+router.register(r'usuarios', UserView, basename='usuario')
 
 custom_urlpatterns = [
-    path("countries/",CountryChoicesView.as_view(),name="country-choices"),
-    path("typedocs/",TypeDocChoicesView.as_view(),name="typedocs-choices"),
+    path('countries/', CountryChoicesView.as_view(), name='country-choices'),
+    path('typedocs/', TypeDocChoicesView.as_view(), name='typedocs-choices'),
 ]
 
-
-#cuando es APIVIEW agregamos aqui
 urlpatterns = [
+    path('password-reset-request/', PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
     path('login/', LoginView.as_view(), name='login'),
-]+ custom_urlpatterns
+] + custom_urlpatterns
 
 urlpatterns += router.urls
